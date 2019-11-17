dbe9c8b81ff99c393ab2235ff101233b75bcffbd
Krisz <kriszrokakaszas@gmail.com>
2019-11-17 15:59:16 +0100

    basic bl complete

---
+++ git_practice/businesslogic.cpp
@@ -0,0 +1,11 @@
+#include "businesslogic.h"
+
+BusinessLogic::BusinessLogic()
+{
+
+}
+
+QString BusinessLogic::GetMessage()
+{
+    return "It Works!!! ^_^";
+}
---
+++ git_practice/businesslogic.h
@@ -0,0 +1,13 @@
+#ifndef BUSINESSLOGIC_H
+#define BUSINESSLOGIC_H
+
+#include <QString>
+
+class BusinessLogic
+{
+public:
+    BusinessLogic();
+    QString GetMessage();
+};
+
+#endif // BUSINESSLOGIC_H
